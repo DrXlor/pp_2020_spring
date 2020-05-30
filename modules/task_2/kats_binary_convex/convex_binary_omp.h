218d7485f07a48c0d3f0090d28b00a8e6ec0db01
Евгений Кац <eugeny@live.ru>
2020-05-31 02:26:38 +0300

    whitespace fix 3

---
+++ modules/task_2/kats_binary_convex/convex_binary_omp.h
@@ -0,0 +1,27 @@
+// Copyright 2020 Kats Eugeny
+#ifndef MODULES_TASK_2_KATS_BINARY_CONVEX_CONVEX_BINARY_OMP_H_
+#define MODULES_TASK_2_KATS_BINARY_CONVEX_CONVEX_BINARY_OMP_H_
+
+#include <omp.h>
+
+#include <algorithm>
+#include <vector>
+
+class SideClass {
+ public:
+  double length(int* start, int* end);
+  double cosinus(int* start, int* end, int* startsecond);
+  int** getRandomPic(const int a, const int b);
+  std::vector<int*> inside(std::vector<int*> vec, int** convex_arr, int a);
+};
+class MainClass {
+ public:
+  std::vector<int*> jarvis(int** convex_arr, int a);
+  std::vector<int*> getComponent(int** arr, int a, int b, int*** convex_arr,
+                                 int* num);
+  std::vector<int*> jarvis_OMP(int** convex_arr, int a, int num_thr);
+  std::vector<int*> getComponent_OMP(int** arr, int a, int b, int*** convex_arr,
+                                     int* num, int num_thr);
+};
+
+#endif  // MODULES_TASK_2_KATS_BINARY_CONVEX_CONVEX_BINARY_OMP_H_
